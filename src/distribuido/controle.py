9d3e1f8ef291cc0ed26db80b8228f029e5a1d7f7
Antonio Aldisio <aldisiofilho@gmail.com>
2022-12-19 16:07:09 -0300

    update controle

--- src/distribuido/controle.py
+++ src/distribuido/controle.py
@@ -8,7 +8,6 @@ from .comandos import comandos
 
 
 def controle(sala: str):
-    comandos(sala)
     status = open_json('src/json/estados.json')
 
     while True:
@@ -28,6 +27,7 @@ def controle(sala: str):
             while(count < 6):
                 status['sala01'][0]['inputs'][count]['status'] = sensor_on_of(status['sala01'][0]['inputs'][count]['gpio'])
                 count += 1
+            comandos('sala01')
 
         if(sala == 'sala02'):
             temp_sala01, umid_sala01 = temperatura('02')
@@ -45,6 +45,7 @@ def controle(sala: str):
             while(count < 6):
                 status['sala02'][0]['inputs'][count]['status'] = sensor_on_of(status['sala02'][0]['inputs'][count]['gpio'])
                 count += 1
+            comandos('sala02')
 
         save_json('src/json/estados.json', status)
         send_status_to_central(status['ip_servidor_central'],
