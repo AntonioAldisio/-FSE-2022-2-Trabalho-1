45019f495b39145aac1268f1a0233b979a82698c
Antonio Aldisio <aldisiofilho@gmail.com>
2022-12-19 17:05:03 -0300

    fix

--- src/central/comandos.py
+++ src/central/comandos.py
@@ -5,9 +5,10 @@ from central.prints import (print_status_sala_01,
 from utils.troca_comando import (put_command,
                                  swap_command)
 from utils.send_comandos import send_comandos
-import os
+from utils.open_json import open_json
 
 def comandos(escolha):
+    data = open_json('src/json/comandos.json')
     if (escolha == 1):
         print('Mostra estado da sala')
         print_status_sala_01()
@@ -18,14 +19,20 @@ def comandos(escolha):
         print('Alarme Ligado')
         put_command('sala01', 4, 'ON')
         put_command('sala02', 4, 'ON')
-        send_comandos()
+        send_comandos(data['ip_sala_01'],
+                      data['porta_sala_01'])
+        send_comandos(data['ip_sala_02'],
+                      data['porta_sala_02'])
         return 'Alarme Ligado'
 
     if (escolha == 3):
         print('Alarme Desligado')
         put_command('sala01', 4, 'OFF')
         put_command('sala02', 4, 'OFF')
-        send_comandos()
+        send_comandos(data['ip_sala_01'],
+                      data['porta_sala_01'])
+        send_comandos(data['ip_sala_02'],
+                      data['porta_sala_02'])
         return 'Alarme Desligado'
 
     if (escolha == 4):
@@ -33,7 +40,8 @@ def comandos(escolha):
         print_inputs()
         escolha_input = int(input())
         swap_command(escolha_input, 'sala01')
-        send_comandos()
+        send_comandos(data['ip_sala_01'],
+                      data['porta_sala_01'])
         return 'Equipamento de input na sala 01 foi selecioando'
 
     if (escolha == 5):
@@ -41,5 +49,6 @@ def comandos(escolha):
         print_inputs()
         escolha_input = int(input())
         swap_command(escolha_input, 'sala02')
-        send_comandos()
+        send_comandos(data['ip_sala_02'],
+                      data['porta_sala_02'])
         return 'Equipamento de input na sala 02 foi selecioando'
