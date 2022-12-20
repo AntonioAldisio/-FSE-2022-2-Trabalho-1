e6b338de63fe4f72c3d266fdccf5c211453b6efd
Antônio Aldísio <aldisiofilho@gmail.com>
2022-12-20 12:36:55 -0300

    remocao de print

--- src/app_servidor_central.py
+++ src/app_servidor_central.py
@@ -4,10 +4,10 @@ from central.servidor_central import servidor_central
 
 if __name__ == "__main__":
     log()
-    print('Informe o caminho do arquivo de configuração da sala 01:')
+    # print('Informe o caminho do arquivo de configuração da sala 01:')
     # sala_01 = input()
-    sala_01 = 'src/json/sala_1.json'
-    print('Informe o caminho do arquivo de configuração da sala 02:')
+    # print('Informe o caminho do arquivo de configuração da sala 02:')
     # sala_02 = input()
+    sala_01 = 'src/json/sala_1.json'
     sala_02 = 'src/json/sala_2.json'
     servidor_central(sala_01, sala_02)
