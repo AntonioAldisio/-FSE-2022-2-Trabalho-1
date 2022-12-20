1f2437b990fe668b4e8b1a83f44074c7c08e99d8
Antonio Aldisio <aldisiofilho@gmail.com>
2022-12-19 23:19:05 -0300

    update servidor

--- src/app_servidor_distribuido.py
+++ src/app_servidor_distribuido.py
@@ -2,8 +2,10 @@ from distribuido.servidor_distrbuido import servidor_distribuido
 
 
 print('Informe o caminho do arquivo de configuração da sala 01:')
-    # sala_01 = input()
-sala_01 = 'src/json/sala_1.json'
-servidor_distribuido(sala_01)
-sala_02 = 'src/json/sala_2.json'
-servidor_distribuido(sala_02)
+sala = input()
+if (sala == '01'):
+    sala_01 = 'src/json/sala_1.json'
+    servidor_distribuido(sala_01)
+else:
+    sala_02 = 'src/json/sala_2.json'
+    servidor_distribuido(sala_02)
