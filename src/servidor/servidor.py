1512cc91e1baa030d6205c51da2142fce15607b8
antonioaldisio <aldsiofilho@gmail.com>
2022-12-21 00:25:52 -0300

    correcao servidor

--- src/servidor/servidor.py
+++ src/servidor/servidor.py
@@ -5,17 +5,23 @@ from utils.save_json import save_json
 
 def initSocket(ip, port, diretorio):
     dir = 'src/json/'+diretorio+'.json'
+    dicionario = ''
     try:
         tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         tcp.bind((ip, port))
-        tcp.listen(3)
+        tcp.listen(2)
         while True:
             con, cliente = tcp.accept()
             print ('Concetado por', cliente)
             while True:
-                msg = con.recv(8192).decode("utf-8")
-                dicionario = json.loads(msg)
-                if not msg: break
-            save_json(dir, dicionario)
+                msg = con.recv(12288)
+                msg = msg.decode()
+                dicionario += msg
+                # dicionario.append(msg)
+                # json_distribuido = json.loads(dicionario)
+                if not msg: 
+                    save_json(dir, json.loads(dicionario))
+                    dicionario = ''
+                    break
     except KeyboardInterrupt:
         sys.exit(0)
