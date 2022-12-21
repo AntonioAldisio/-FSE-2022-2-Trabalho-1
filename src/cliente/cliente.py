3ea81d2f8cb28ef22eb93cab05414b0388991d75
antonioaldisio <aldsiofilho@gmail.com>
2022-12-20 22:05:22 -0300

    adequacao do cliente

--- src/cliente/cliente.py
+++ src/cliente/cliente.py
@@ -2,17 +2,16 @@
 
 import socket
 import json
+from utils.open_json import open_json
 
 
 def client(ip:str, port:int, dir:str):
-    with open(dir) as json_file:
-        data = json.load(json_file)
-
+    data = open_json(dir)
     #  create a socket object
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #  connect the client
     client.connect((ip, port))
     #  send some data
-    client.sendall(bytes(json.dumps(data), 'UTF-8'))
+    client.sendall(bytes(json.dumps(data), encoding="UTF-8"))
     #  receive data from the server
     client.close()
